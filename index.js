8d17ad997b32c38bbe824305fb314f32ff21c10d
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2024-11-29 00:23:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post("/webhook", async (req, res) => {
           const {data: member} = await memberstack.members.retrieve({ id: found.id });
           console.log("Member found", member);
           const json = member?.json ?? { products: [], orders: [] };
-          if (!Object.keys(json.orders).includes(order_id)) {
+          if (!Object.keys(json.orders ?? {}).includes(order_id)) {
             json.orders[order_id] = {
               items: order.lineItems.map((item) => ({ name: item.name, quantity: item.quantity, amount: item.totalMoney.amount.toString() })),
               date: new Date().toISOString(),
